a289afb1f98168637f9dd584a70556d332081088
Nazih Ahmed <nazih.dev@gmail.com>
2018-08-11 00:54:17 +0300

    clean

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def handle_message(message):
 
 @socketio.on('json')
 def handle_json(json):
-    print('received json: ' + str(json))
+    print('received json1: ' + str(json))
 
 @socketio.on('my event')
 def handle_my_custom_event(json):
