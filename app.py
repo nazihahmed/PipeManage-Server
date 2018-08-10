edd98c173c6d5fa352a568c51b09dbce139170a3
Nazih Ahmed <nazih.dev@gmail.com>
2018-08-10 16:31:28 +0300

    add control page

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ import RPi.GPIO as GPIO
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 
+pins = {
+   17 : {'name' : 'coffee maker', 'state' : GPIO.LOW},
+   27 : {'name' : 'lamp', 'state' : GPIO.LOW}
+   }
+
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.LOW)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -40,6 +49,52 @@ def readPin(pin):
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('page.html', name=name)
+
+@app.route("/control")
+def control():
+   # For each pin, read the pin state and store it in the pins dictionary:
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+   # Put the pin dictionary into the template data dictionary:
+   templateData = {
+      'pins' : pins
+      }
+   # Pass the template data into the template main.html and return it to the user
+   return render_template('control.html', **templateData)
+
+# The function below is executed when someone requests a URL with the pin number and action in it:
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+   # Convert the pin from the URL into an integer:
+   changePin = int(changePin)
+   # Get the device name for the pin being changed:
+   deviceName = pins[changePin]['name']
+   # If the action part of the URL is "on," execute the code indented below:
+   if action == "on":
+      # Set the pin high:
+      GPIO.output(changePin, GPIO.HIGH)
+      # Save the status message to be passed into the template:
+      message = "Turned " + deviceName + " on."
+   if action == "off":
+      GPIO.output(changePin, GPIO.LOW)
+      message = "Turned " + deviceName + " off."
+   if action == "toggle":
+      # Read the pin and set it to whatever it isn't (that is, toggle it):
+      GPIO.output(changePin, not GPIO.input(changePin))
+      message = "Toggled " + deviceName + "."
+
+   # For each pin, read the pin state and store it in the pins dictionary:
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   templateData = {
+      'message' : message,
+      'pins' : pins
+   }
+
+   return render_template('control.html', **templateData)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
