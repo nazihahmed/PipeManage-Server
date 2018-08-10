d9b157a2eac77ece0f64f741466e900c93811a45
Nazih Ahmed <nazih.dev@gmail.com>
2018-08-11 00:51:29 +0300

    add logs

--- app.py
+++ app.py
@@ -25,6 +25,14 @@ def index():
 def handle_message(message):
     print('received message: ' + message)
 
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
 @app.route('/main')
 def hello2():
    now = datetime.datetime.now()
