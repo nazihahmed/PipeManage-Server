1958f6987f219f1763f1034d20d8327a5d185095
Nazih Ahmed <nazih.dev@gmail.com>
2018-08-11 22:56:27 +0300

    basic vue

--- static/main.js
+++ static/main.js
@@ -1,6 +1,13 @@
-var app = new Vue({
-  el: '#app',
+var app2 = new Vue({
+  el: '#app-2',
   data: {
-    message: 'Hello Vue!'
+    message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 })
+var url = 'http://' + document.domain + ':' + location.port;
+var socket = io.connect(url);
+console.log("connect to socket",url)
+socket.on('connect', function() {
+    console.log("socket connected");
+    socket.emit('my event', {data: 'I\'m connected!'});
+});
