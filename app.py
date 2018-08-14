f7dd5f42103ab48ecb7237e878786b12268a7bf4
Nazih Ahmed <nazih.dev@gmail.com>
2018-08-15 00:00:59 +0300

    basic components

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+import eventlet
 import datetime
 # import RPi.GPIO as GPIO
 from flask_socketio import SocketIO
@@ -146,6 +147,39 @@ def update():
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
 
 if __name__ == '__main__':
     socketio.run(app)
