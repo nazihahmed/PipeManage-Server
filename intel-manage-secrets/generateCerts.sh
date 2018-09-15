b5052139fbbedfcf6ef5202da22ee83848facb89
nazih ahmed <nazih.dev@gmail.com>
2018-09-15 22:15:42 +0300

    fix script

--- intel-manage-secrets/generateCerts.sh
+++ intel-manage-secrets/generateCerts.sh
@@ -4,7 +4,7 @@ if [ ! -d "$DIR" ]; then
   mkdir "$DIR"
   cp root.pem $DIR
   openssl genrsa -out $DIR/deviceCert.key 2048
-  openssl req -subj "//C=IL/O=Makovation" -new -key $DIR/deviceCert.key -out deviceCert.csr
+  openssl req -subj "/C=IL/O=Makovation" -new -key $DIR/deviceCert.key -out deviceCert.csr
   openssl x509 -req -in deviceCert.csr -CA sampleCACertificate.pem -CAkey sampleCACertificate.key -CAcreateserial -out deviceCert.crt -days 3650 -sha256
   cat deviceCert.crt sampleCACertificate.pem > $DIR/deviceCert.crt
   rm deviceCert.*
