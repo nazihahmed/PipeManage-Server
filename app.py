29075b2dbb1c83d0dcba3490fbfec9fbb811c648
nazih ahmed <nazih.dev@gmail.com>
2018-09-15 22:45:51 +0300

    pprint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 # import eventlet
 import datetime
-import json
+import pprint
 import RPi.GPIO as GPIO
 # from flask_socketio import SocketIO
 # import subprocess
@@ -38,7 +38,7 @@ def customCallback(data1,data2,data3):
 
 myShadowClient.connect()
 # Create a device shadow instance using persistent subscription
-json.dumps(myShadowClient, indent=1)
+pprint.pprint(myShadowClient)
 # myDeviceShadow = myShadowClient.createShadowHandlerWithName("Bot", True)
 # # Shadow operations
 # print("get Shadow")
