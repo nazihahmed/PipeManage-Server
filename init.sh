429e2cdcb5cce6b331fd99ef3504f41637ac3c10
Nazih Ahmed <nazih.dev@gmail.com>
2018-09-19 00:22:51 +0300

    add init for thingName file

--- init.sh
+++ init.sh
@@ -1,3 +1,13 @@
 python3 -m venv venv
 . venv/bin/activate
 python3 -m pip install -r requirements.txt
+DIR="certs"
+FILE="thingName.txt"
+thingFile="$DIR/$FILE"
+if [ -d "$DIR" ]; then
+  if [ ! -f "$thingFile" ]; then
+      pushd "$DIR"
+        touch $FILE
+      popd
+  fi
+fi
