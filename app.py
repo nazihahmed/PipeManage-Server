a43a32dde961094410b830c8df4e5145f5db3c86
Nazih Ahmed <nazih.dev@gmail.com>
2018-11-17 15:20:04 +0200

    a+

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ DEBUG = True
 
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 thingFileName = os.path.join(fileDir, 'certs/thingName.txt')
-thingFile = open(thingFileName, 'r+')
+thingFile = open(thingFileName, 'a+')
 thingName = ''
 if os.stat(thingFileName).st_size == 0:
     response = client.scan(
