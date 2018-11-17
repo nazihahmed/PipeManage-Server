ae967f85f50c5b441c27c70d136aa473d92f877b
Nazih Ahmed <nazih.dev@gmail.com>
2018-11-17 17:56:07 +0200

    fix the run

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-. venv/bin/activate
-python3 -m pip install -r requirements.txt
-pushd /opt/intel/intel-manage-secrets
-  bash ./generateCerts.sh
+pushd /opt/intel
+  . venv/bin/activate
+  python3 -m pip install -r requirements.txt
+  pushd ./intel-manage-secrets
+    bash ./generateCerts.sh
+  popd
+  python3 app.py | tee dev_app.log
 popd
-python3 app.py | tee dev_app.log
